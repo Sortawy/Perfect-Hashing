3f5993b0a947c9a3e7997b6e5d10db59cb92ce4c
ahmedyoussefg <a.goerany@gmail.com>
2024-04-22 00:54:09 +0200

    Change old main (@mahfouz72) and create new main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,26 +1,8 @@
 package src.main;
 
 public class Main {
-    public static void main(String[] args) {
-       HashTable<Integer> hashTable = new QuadraticSpaceHashTable<>(10);
+    public static void main(String[] args)
+    {
 
-       for (int i = 0; i < 10; i++) {
-           hashTable.insert(i);
-       }
-       hashTable.delete(9);
-       hashTable.delete(8);
-       hashTable.insert(11);
-       hashTable.insert(11);
-       System.out.println(hashTable);
-       for (int i = 0; i < 10; i++) {
-           if (!hashTable.contains(i)) {
-               System.out.println("Error" + i);
-           }
-       }
-       for (int i = 10; i < 20; i++) {
-           if (hashTable.contains(i)) {
-               System.out.println("Error" + i);
-           }
-       }
     }
 }
