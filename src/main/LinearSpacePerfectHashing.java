513f7292af0b0651ab0ce09522a04b1ccd24a407
MouradMahgoub <mouradmahgoub710@gmail.com>
2024-04-22 23:41:03 +0200

    add insert and delete methods

--- src/main/LinearSpacePerfectHashing.java
+++ src/main/LinearSpacePerfectHashing.java
@@ -3,91 +3,142 @@ package src.main;
 public class LinearSpacePerfectHashing <T> {
     private UniversalHashing<T> firstLevelHashFunction;
     private UniversalHashing<T>[] secondLevelHashFunctions;
+    private T[] keys;
     private int numberOfKeys;
-    private int numberOfRebuild;
-    private int[] numberOfCollisions;
+    private int numberOfInsertions;
+    private int numberOfDeletions;
+    private int numberOfRehashing;
+    private int[] firstLevelHashTable;
     private T[][] secondLevelHashTables;
 
     public LinearSpacePerfectHashing (T[] keys) {
-        this.numberOfRebuild = 0;
+        this.numberOfInsertions = 0;
+        this.numberOfDeletions = 0;
+        this.numberOfRehashing = 0;
         this.numberOfKeys = keys.length;
-        this.numberOfCollisions = new int[numberOfKeys];
+        this.keys = keys;
+        this.buildHashTable();
+    }
+
+    private void buildHashTable () {
+        this.firstLevelHashTable = new int[numberOfKeys];
         this.secondLevelHashTables = (T[][]) new Object[numberOfKeys][];
         this.firstLevelHashFunction = new UniversalHashing<T>(this.numberOfKeys);
         this.secondLevelHashFunctions = (UniversalHashing<T>[]) new UniversalHashing<?>[this.numberOfKeys];
-        this.buildHashTable(keys);
+        this.buildFirstLevelHashTable();
+        this.buildSecondLevelHashTables();
     }
 
-    private void buildHashTable (T[] keys) {
-        this.buildNumberOfCollisions(keys);
-        this.buildSecondLevelHashTables(keys);
-    }
-
-    private void buildNumberOfCollisions (T[] keys) {
+    private void buildFirstLevelHashTable () {
         for(int i=0; i<this.numberOfKeys; i++){
             T key = keys[i];
-            int hashValue = this.firstLevelHashFunction.hash(key);
-            this.numberOfCollisions[hashValue]++;
+            int firstLevelIndex = this.firstLevelHashFunction.hash(key);
+            this.firstLevelHashTable[firstLevelIndex]++;
         }
     }
 
-    private void buildSecondLevelHashTables (T[] keys) {
-        for(int i=0; i<this.secondLevelHashTables.length; i++){
-            if(numberOfCollisions[i] == 0) continue;
-            this.secondLevelHashTables[i] =  (T[]) new Object[this.numberOfCollisions[i]*this.numberOfCollisions[i]];
-            this.secondLevelHashFunctions[i] = new UniversalHashing<T>(this.numberOfCollisions[i]*this.numberOfCollisions[i]);
-            this.buildHashTable(keys, i);
+    private void buildSecondLevelHashTables () {
+        for(int firstLevelIndex=0; firstLevelIndex<this.secondLevelHashTables.length; firstLevelIndex++){
+            if(firstLevelHashTable[firstLevelIndex] == 0) continue;
+            this.secondLevelHashTables[firstLevelIndex] =  (T[]) new Object[this.firstLevelHashTable[firstLevelIndex]*this.firstLevelHashTable[firstLevelIndex]];
+            this.secondLevelHashFunctions[firstLevelIndex] = new UniversalHashing<T>(this.firstLevelHashTable[firstLevelIndex]*this.firstLevelHashTable[firstLevelIndex]);
+            this.buildHashTableEntry(firstLevelIndex);
         }
     }
 
-    private void buildHashTable (T[] keys, int i) {
+    private void buildHashTableEntry (int firstLevelIndex) {
         while(true){
-            boolean[] isUsed = new boolean[this.numberOfCollisions[i]*this.numberOfCollisions[i]];
+            boolean[] isUsed = new boolean[this.firstLevelHashTable[firstLevelIndex]*this.firstLevelHashTable[firstLevelIndex]];
             boolean isGood = true;
             for(int j=0; j<this.numberOfKeys; j++){
                 T key = keys[j];
-                int hashValue = this.secondLevelHashFunctions[i].hash(key);
-                if(firstLevelHashFunction.hash(key) == i){
-                    if(isUsed[hashValue]){
+                int secondLevelIndex = this.secondLevelHashFunctions[firstLevelIndex].hash(key);
+                if(firstLevelHashFunction.hash(key) == firstLevelIndex){
+                    if(isUsed[secondLevelIndex]){
                         isGood = false;
                         break;
                     }
-                    isUsed[hashValue] = true;
-                    this.secondLevelHashTables[i][hashValue] = key;
+                    isUsed[secondLevelIndex] = true;
+                    this.secondLevelHashTables[firstLevelIndex][secondLevelIndex] = key;
                 }
             }
             if(isGood) break;
-            this.rebuildHashTable(i);
+            this.rebuildHashTableEntry (firstLevelIndex);
         }
     }
 
-    private void rebuildHashTable (int i) {
-        this.numberOfRebuild++;
-        this.secondLevelHashFunctions[i].regenerateHashFunction();
+    private void rebuildHashTableEntry (int firstLevelIndex) {
+        this.numberOfRehashing++;
+        this.secondLevelHashFunctions[firstLevelIndex].regenerateHashFunction();
     }
 
-    public boolean lookUp (T key) {
+    public void insert (T key) {
+        if(this.contains(key)) return;
+        this.numberOfInsertions++;
+        T[] newKeys = (T[]) new Object[this.keys.length+1];
+        for(int i=0; i<this.keys.length; i++)
+            newKeys[i] = this.keys[i];
+        newKeys[this.keys.length] = key;
+        this.keys = newKeys;
+        this.numberOfKeys++;
+        this.buildHashTable();
+        // int firstLevelIndex = this.firstLevelHashFunction.hash(key);
+        // int secondLevelIndex = this.secondLevelHashFunctions[firstLevelIndex].hash(key);
+        // if(this.secondLevelHashTables[firstLevelIndex][secondLevelIndex] == null){
+        //     this.secondLevelHashTables[firstLevelIndex][secondLevelIndex] = key;
+        //     this.numberOfInsertions++;
+        // }else{
+        //     // this.rebuildHashTableEntry(firstLevelIndex);
+        //     // this.insert(key);
+        // }
+    }
+
+    public void delete (T key) {
+        if(!this.contains(key)) return;
+        this.numberOfDeletions++;
+        T[] newKeys = (T[]) new Object[this.keys.length-1];
+        int index = 0;
+        for(int i=0; i<this.keys.length; i++){
+            if(this.keys[i] != key){
+                newKeys[index] = this.keys[i];
+                index++;
+            }
+        }
+        this.keys = newKeys;
+        this.numberOfKeys--;
+        this.buildHashTable();
+        // int firstLevelIndex = this.firstLevelHashFunction.hash(key);
+        // int secondLevelIndex = this.secondLevelHashFunctions[firstLevelIndex].hash(key);
+        // if(this.secondLevelHashTables[firstLevelIndex][secondLevelIndex] == key){
+        //     this.secondLevelHashTables[firstLevelIndex][secondLevelIndex] = null;
+        //     this.numberOfDeletions++;
+        // }
+    }
+
+    public boolean contains (T key) {
         try {
-            int hashValue = this.firstLevelHashFunction.hash(key);
-            int secondLevelHashValue = this.secondLevelHashFunctions[hashValue].hash(key);
-            return key == this.secondLevelHashTables[hashValue][secondLevelHashValue];
+            int firstLevelIndex = this.firstLevelHashFunction.hash(key);
+            int secondLevelIndex = this.secondLevelHashFunctions[firstLevelIndex].hash(key);
+            return key == this.secondLevelHashTables[firstLevelIndex][secondLevelIndex];
         } catch (Exception e) {
             return false;
         }
     }
 
-    public int getNumberOfRebuild () {
-        return this.numberOfRebuild;
+    public int getNumberOfRehashing () {
+        return this.numberOfRehashing;
     }
 
     public static void main(String[] args) {
         Integer[] keys = {1, 2, 3, 4, 5, 10, 6, 7, 8, 9};
         LinearSpacePerfectHashing<Integer> lsh = new LinearSpacePerfectHashing<>(keys);
-        System.out.println(lsh.lookUp(1)); // 0
-        System.out.println(lsh.lookUp(10)); // 1
-        System.out.println(lsh.lookUp(7)); // 6
-        System.out.println(lsh.lookUp(0)); // 1
-        System.out.println(lsh.lookUp(9)); // 8
+        System.out.println(lsh.contains(1)); // 0
+        lsh.delete(1);
+        System.out.println(lsh.contains(1)); // 0
+        System.out.println("-------------------------------------------------"); // 0
+        System.out.println(lsh.contains(0)); // 1
+        lsh.insert(0);
+        System.out.println(lsh.contains(0)); // 1
     }
     
 }
